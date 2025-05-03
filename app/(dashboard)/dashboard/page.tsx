89e1103a24f0dba7a840c2cfd0cf8cda1dd59dfb
Isuu1 <jakub.babelek@gmail.com>
2025-05-03 16:49:27 +0100

    Replaced data in AccountsProvider from db -> bank account

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -11,17 +11,18 @@ import { useUser } from "@/shared/providers/UserProvider";
 import { MdAccountBalance } from "react-icons/md";
 import { FaCreditCard } from "react-icons/fa";
 import PlaidLink from "@/shared/components/PlaidLink/PlaidLink";
+import AccountsTile from "@/features/dashboard/components/AccountsTile";
 
 export default function Page() {
   const { user } = useUser();
   console.log("User in dashboard page:", user);
 
   const tilesInUse = [
-    // {
-    //   name: "Accounts",
-    //   component: <AccountsTile />,
-    //   icon: <MdAccountBalance />,
-    // },
+    {
+      name: "Accounts",
+      component: <AccountsTile />,
+      icon: <MdAccountBalance />,
+    },
     {
       name: "Cards",
       component: <CardsTile />,
--- features/dashboard/components/CardsTile.tsx
+++ features/dashboard/components/CardsTile.tsx
@@ -1,49 +1,48 @@
 "use client";
 
-import { useAccounts } from "@/shared/providers/AccountsProvider";
+//import { useAccounts } from "@/shared/providers/AccountsProvider";
 import React from "react";
 
 //Styles
 import styles from "./CardsTile.module.scss";
 //Icons
-import { PiContactlessPaymentFill } from "react-icons/pi";
-import { IoTrashBin } from "react-icons/io5";
+//import { PiContactlessPaymentFill } from "react-icons/pi";
+//import { IoTrashBin } from "react-icons/io5";
 
 const CardsTile = () => {
-  const { accounts } = useAccounts();
+  //const { accounts } = useAccounts();
 
-  const userCards = accounts?.reduce<
-    {
-      accountId: string;
-      id: number;
-      card_number: string;
-      expiration_date: string;
-      card_type: string;
-      balance: number;
-    }[]
-  >((acc, account) => {
-    const cards = account.cards.map((card) => ({
-      ...card,
-      accountId: account.id,
-    }));
-    return [...acc, ...cards];
-  }, []);
+  // const userCards = accounts?.reduce<
+  //   {
+  //     accountId: string;
+  //     id: number;
+  //     card_number: string;
+  //     expiration_date: string;
+  //     card_type: string;
+  //     balance: number;
+  //   }[]
+  // >((acc, account) => {
+  //   const cards = account.cards.map((card) => ({
+  //     ...card,
+  //     accountId: account.id,
+  //   }));
+  //   return [...acc, ...cards];
+  // }, []);
 
-  console.log("Cards in CardsTile:", userCards);
+  // console.log("Cards in CardsTile:", userCards);
 
-  if (!accounts || !userCards) {
-    return <div>Loading...</div>;
-  }
+  // if (!accounts || !userCards) {
+  //   return <div>Loading...</div>;
+  // }
 
   return (
     <div>
       <div className={styles.cards}>
-        {userCards.map((card) => (
+        {/* {userCards.map((card) => (
           <div key={card.id} className={styles.cardWrapper}>
             <div className={styles.cardOverlay}></div>
             <div className={styles.card}>
               <div className={styles.type}>
-                {/* <p>{card.card_type}</p> */}
                 <PiContactlessPaymentFill className={styles.icon} />
               </div>
               <div className={styles.details}>
@@ -55,7 +54,7 @@ const CardsTile = () => {
               <IoTrashBin />
             </div>
           </div>
-        ))}
+        ))} */}
       </div>
     </div>
   );
