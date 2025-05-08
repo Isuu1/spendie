4bfed41c99868782b3656300e0b0c3bd20812079
Isuu1 <jakub.babelek@gmail.com>
2025-05-08 02:34:31 +0100

    Move future balance to separate component

---
+++ features/total-balance/components/FutureBalance.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import React, { useState } from "react";
+
+//Styles
+import styles from "./FutureBalance.module.scss";
+import moment from "moment";
+import { StandingOrder } from "@/shared/types/standing-order";
+
+interface FutureBalanceProps {
+  latestStandingOrderIncome: StandingOrder | null;
+  latestStandingOrderExpense: StandingOrder | null;
+  totalBalance: number;
+}
+
+const FutureBalance: React.FC<FutureBalanceProps> = ({
+  latestStandingOrderIncome,
+  latestStandingOrderExpense,
+  totalBalance,
+}) => {
+  const [displayedFutureBalance, setDisplayedFutureBalance] =
+    useState<string>("end of the month");
+
+  const startOfMonth = moment().startOf("month");
+  const endOfMonth = moment().endOf("month");
+
+  const calculateFutureBalance = () => {
+    if (!latestStandingOrderIncome && !latestStandingOrderExpense) {
+      return totalBalance; // No income date available
+    }
+
+    let incomeAmountForCalculation = 0;
+    let expenseAmountForCalculation = 0;
+    if (latestStandingOrderIncome && latestStandingOrderExpense) {
+      const incomeDate = moment(new Date(latestStandingOrderIncome.date));
+      const expenseDate = moment(new Date(latestStandingOrderExpense.date));
+      if (displayedFutureBalance === "end of the month") {
+        if (incomeDate.isBetween(startOfMonth, endOfMonth, null, "[]")) {
+          //'[]' includes start and end dates
+          incomeAmountForCalculation = latestStandingOrderIncome?.amount ?? 0;
+        }
+
+        if (expenseDate.isBetween(startOfMonth, endOfMonth, null, "[]")) {
+          //'[]' includes start and end dates
+          expenseAmountForCalculation = latestStandingOrderExpense?.amount ?? 0;
+        }
+      }
+      if (displayedFutureBalance === "after next income") {
+        if (incomeDate.isAfter(moment(), "day")) {
+          incomeAmountForCalculation = latestStandingOrderIncome?.amount ?? 0;
+        }
+
+        if (expenseDate.isAfter(moment(), "day")) {
+          expenseAmountForCalculation = latestStandingOrderExpense?.amount ?? 0;
+        }
+      }
+    }
+
+    return (
+      totalBalance + incomeAmountForCalculation - expenseAmountForCalculation
+    );
+  };
+
+  const handleFutureBalanceChange = (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const selectedValue = e.target.value;
+    setDisplayedFutureBalance(selectedValue);
+  };
+
+  return (
+    <div className={styles.futureBalance}>
+      <div className={styles.range}>
+        <label htmlFor="range">Time period</label>
+        <select
+          id="range"
+          name="range"
+          className={styles.select}
+          onChange={handleFutureBalanceChange}
+        >
+          <option value="end of the month">End of the month</option>
+          <option value="after next income">After next income</option>
+        </select>
+      </div>
+      {/* {displayedFutureBalance === "end of the month" && (
+          <p className={styles.rangeSubtitle}>
+            {moment().endOf("month").format("DD MMMM YYYY")}
+          </p>
+        )}
+        {displayedFutureBalance === "after next income" && (
+          <p className={styles.rangeSubtitle}>
+            {moment(new Date(latestStandingOrderIncome.date)).format(
+              "DD MMMM YYYY"
+            )}
+          </p>
+        )} */}
+      <h2 className={styles.value}>£{calculateFutureBalance().toFixed(2)}</h2>
+    </div>
+  );
+};
+
+export default FutureBalance;
--- features/total-balance/components/TotalBalanceTile.tsx
+++ features/total-balance/components/TotalBalanceTile.tsx
@@ -10,13 +10,11 @@ import { FaLongArrowAltDown } from "react-icons/fa";
 import { useUser } from "@/shared/providers/UserProvider";
 import { useAccounts } from "@/shared/providers/AccountsProvider";
 import { getLatestStandingOrder } from "../lib/utils";
+import FutureBalance from "./FutureBalance";
 
 const TotalBalanceTile = () => {
   const { accounts } = useAccounts();
   const { user } = useUser();
-
-  // const [displayedFutureBalance, setDisplayedFutureBalance] =
-  //   useState<string>("end of the month");
 
   if (!accounts) {
     return <p>Loading...</p>;
@@ -30,83 +28,14 @@ const TotalBalanceTile = () => {
   const latestStandingOrderIncome = getLatestStandingOrder("income", user);
   const latestStandingOrderExpense = getLatestStandingOrder("expense", user);
 
-  // const startOfMonth = moment().startOf("month");
-  // const endOfMonth = moment().endOf("month");
-
-  // const calculateFutureBalance = () => {
-  //   if (!latestStandingOrderIncome && !latestStandingOrderExpense) {
-  //     return; // No income date available
-  //   }
-  //   const incomeDate = moment(new Date(latestStandingOrderIncome.date));
-  //   const expenseDate = moment(new Date(latestStandingOrderExpense.date));
-  //   let incomeAmountForCalculation = 0;
-  //   let expenseAmountForCalculation = 0;
-  //   if (displayedFutureBalance === "end of the month") {
-  //     if (incomeDate.isBetween(startOfMonth, endOfMonth, null, "[]")) {
-  //       //'[]' includes start and end dates
-  //       incomeAmountForCalculation = latestStandingOrderIncome.amount ?? 0;
-  //     }
-
-  //     if (expenseDate.isBetween(startOfMonth, endOfMonth, null, "[]")) {
-  //       //'[]' includes start and end dates
-  //       expenseAmountForCalculation = latestStandingOrderExpense.amount ?? 0;
-  //     }
-  //   }
-  //   if (displayedFutureBalance === "after next income") {
-  //     if (incomeDate.isAfter(moment(), "day")) {
-  //       incomeAmountForCalculation = latestStandingOrderIncome.amount ?? 0;
-  //     }
-
-  //     if (expenseDate.isAfter(moment(), "day")) {
-  //       expenseAmountForCalculation = latestStandingOrderExpense.amount ?? 0;
-  //     }
-  //   }
-
-  //   return (
-  //     totalBalance + incomeAmountForCalculation - expenseAmountForCalculation
-  //   );
-  // };
-
-  const handleFutureBalanceChange = (
-    e: React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    const selectedValue = e.target.value;
-    console.log("Selected value:", selectedValue);
-    //setDisplayedFutureBalance(selectedValue);
-  };
-
   return (
     <div className={styles.totalBalanceTile}>
       <h1 className={styles.balance}>£{totalBalance ?? 0}</h1>
-
-      <div className={styles.futureBalance}>
-        <div className={styles.range}>
-          <label htmlFor="range">Time period</label>
-          <select
-            id="range"
-            name="range"
-            className={styles.select}
-            onChange={handleFutureBalanceChange}
-          >
-            <option value="end of the month">End of the month</option>
-            <option value="after next income">After next income</option>
-          </select>
-        </div>
-        {/* {displayedFutureBalance === "end of the month" && (
-          <p className={styles.rangeSubtitle}>
-            {moment().endOf("month").format("DD MMMM YYYY")}
-          </p>
-        )}
-        {displayedFutureBalance === "after next income" && (
-          <p className={styles.rangeSubtitle}>
-            {moment(new Date(latestStandingOrderIncome.date)).format(
-              "DD MMMM YYYY"
-            )}
-          </p>
-        )} */}
-        {/* <h2 className={styles.value}>£{calculateFutureBalance().toFixed(2)}</h2> */}
-      </div>
-
+      <FutureBalance
+        latestStandingOrderIncome={latestStandingOrderIncome}
+        latestStandingOrderExpense={latestStandingOrderExpense}
+        totalBalance={totalBalance}
+      />
       <div className={styles.stats}>
         <h3 className={styles.title}>Incoming changes</h3>
         <div className={styles.income}>
