2da51a3e7f0e685495d94d36249f70d948f315f2
Isuu1 <jakub.babelek@gmail.com>
2025-05-16 19:14:31 +0100

    Add collapsed state in Sidebar

--- features/dashboard/components/Sidebar.tsx
+++ features/dashboard/components/Sidebar.tsx
@@ -8,10 +8,19 @@ import { TbLayoutDashboardFilled } from "react-icons/tb";
 import { BsWalletFill } from "react-icons/bs";
 import { IoSettings } from "react-icons/io5";
 import { MdManageAccounts } from "react-icons/md";
+import { useState } from "react";
 
 export default function Sidebar() {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  console.log("isCollapsed", isCollapsed);
+
   return (
-    <div className={styles.sidebar}>
+    <div
+      className={styles.sidebar}
+      onMouseEnter={() => setIsCollapsed(true)}
+      onMouseLeave={() => setIsCollapsed(false)}
+    >
       <ul className={styles.menu}>
         <li className={styles.sidebarItem}>
           <TbLayoutDashboardFilled />
