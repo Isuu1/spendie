93a120350f82dea48416058523aeb2708299318f
Isuu1 <jakub.babelek@gmail.com>
2025-05-20 17:31:41 +0100

    Add links to pages in Sidebar

--- features/dashboard/components/Sidebar.tsx
+++ features/dashboard/components/Sidebar.tsx
@@ -6,27 +6,39 @@ import styles from "./Sidebar.module.scss";
 import { TbLayoutDashboardFilled } from "react-icons/tb";
 import { IoWallet } from "react-icons/io5";
 import { FaCalculator } from "react-icons/fa6";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Sidebar() {
-  const pathname = window.location.pathname;
+  const pathname = usePathname();
 
   return (
     <div className={styles.sidebar}>
       <ul className={`${styles.menu}`}>
-        <li
-          className={`${styles.sidebarItem} ${pathname === "/dashboard" ? styles.active : ""}`}
-        >
-          <TbLayoutDashboardFilled />
-          <span className={styles.label}>Dashboard</span>
-        </li>
-        <li className={styles.sidebarItem}>
-          <IoWallet />
-          <span className={styles.label}>Transactions</span>
-        </li>
-        <li className={styles.sidebarItem}>
-          <FaCalculator />
-          <span className={styles.label}>Budget planner</span>
-        </li>
+        <Link href="/dashboard">
+          <li
+            className={`${styles.sidebarItem} ${pathname === "/dashboard" ? styles.active : ""}`}
+          >
+            <TbLayoutDashboardFilled />
+            <span className={styles.label}>Dashboard</span>
+          </li>
+        </Link>
+        <Link href="/transactions">
+          <li
+            className={`${styles.sidebarItem} ${pathname === "/transactions" ? styles.active : ""}`}
+          >
+            <IoWallet />
+            <span className={styles.label}>Transactions</span>
+          </li>
+        </Link>
+        <Link href="/budget-planner">
+          <li
+            className={`${styles.sidebarItem} ${pathname === "/budget-planner" ? styles.active : ""}`}
+          >
+            <FaCalculator />
+            <span className={styles.label}>Budget planner</span>
+          </li>
+        </Link>
       </ul>
     </div>
   );
