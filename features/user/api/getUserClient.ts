db87005afd745e07b8b7b8b2302decf7d9509e60
Isuu1 <jakub.babelek@gmail.com>
2025-06-14 20:06:31 +0100

    Fix typo

--- features/user/api/getUserClient.ts
+++ features/user/api/getUserClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@/supabase/client";
 
-export async function getUserServer() {
+export async function getUserClient() {
   const supabase = createClient();
   const { data: user, error } = await supabase.auth.getUser();
   if (error) {
