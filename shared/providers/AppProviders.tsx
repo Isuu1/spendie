d0e55c170831d2dc88ac0a94c8df8b9b31547dad
Isuu1 <jakub.babelek@gmail.com>
2025-06-14 13:17:49 +0100

    Remove transactions provider

--- shared/providers/AppProviders.tsx
+++ shared/providers/AppProviders.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { UserProvider } from "./UserProvider";
-import { TransactionsProvider } from "./TransactionsProvider";
 import { QueryProvider } from "./QueryProvider";
 
 const AppProviders = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <UserProvider>
-        <QueryProvider>
-          <TransactionsProvider>{children}</TransactionsProvider>
-        </QueryProvider>
+        <QueryProvider>{children}</QueryProvider>
       </UserProvider>
     </>
   );
