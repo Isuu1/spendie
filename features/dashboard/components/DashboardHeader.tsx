e4bc1415e2a5f225e11053fbb09583a79023f51e
Isuu1 <jakub.babelek@gmail.com>
2025-09-13 15:10:17 +0100

    Style DashboardHeader + add class for all svg elements

--- features/dashboard/components/DashboardHeader.tsx
+++ features/dashboard/components/DashboardHeader.tsx
@@ -1,23 +1,55 @@
-import { user } from "@/data/user";
+"use client";
+
+//import { user } from "@/data/user";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 
 //Icons
 import { IoNotifications } from "react-icons/io5";
 //Styles
 import styles from "./DashboardHeader.module.scss";
+import { createClient } from "@/supabase/client";
+import { UserProfile } from "@/features/user/types/user";
 
 const DashboardHeader = () => {
+  const [user, setUser] = React.useState<UserProfile | null>(null);
+
+  const supabase = createClient();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data, error } = await supabase.auth.getUser();
+      if (error) {
+        console.error("Error fetching user:", error);
+      } else {
+        console.log("User data:", data);
+      }
+      const user = await supabase
+        .from("profiles")
+        .select("*")
+        .eq("id", data.user?.id)
+        .single();
+      console.log("Profile data:", user);
+      setUser(user.data);
+    };
+
+    fetchUser();
+  }, [supabase]);
+
   return (
     <div className={styles.header}>
-      <IoNotifications />
+      <p className={styles.welcomeMessage}>Welcome, {user?.username}</p>
       <Image
         className={styles.avatar}
-        src={user.avatar}
+        src="https://i.pravatar.cc/150?img=3"
         alt=""
-        width={40}
-        height={40}
+        width={27}
+        height={27}
       />
+      <i className={styles.notificationsIcon}>
+        <IoNotifications />
+        <span className={styles.counter}></span>
+      </i>
     </div>
   );
 };
