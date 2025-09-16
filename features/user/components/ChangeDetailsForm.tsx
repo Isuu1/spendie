bae9179a9776294df1756039ec21bdea1bc9ef03
Isuu1 <jakub.babelek@gmail.com>
2025-09-16 01:50:47 +0100

    ChangeDetailsForm edit mode + buttons

--- features/user/components/ChangeDetailsForm.tsx
+++ features/user/components/ChangeDetailsForm.tsx
@@ -1,21 +1,38 @@
+"use client";
+
+import React, { useRef, useState } from "react";
+//Components
 import Form from "@/shared/components/ui/Form";
 import Input from "@/shared/components/ui/Input";
-import React from "react";
-import { UserProfile } from "../types/user";
+import Button from "@/shared/components/ui/Button";
+//Types
+import { UserProfile } from "@/features/user/types/user";
 
 interface ChangeDetailsFormProps {
   user: UserProfile;
 }
 
 const ChangeDetailsForm: React.FC<ChangeDetailsFormProps> = ({ user }) => {
+  const [editMode, setEditMode] = useState(false);
+
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const handleCloseForm = () => {
+    setEditMode(false);
+    if (formRef.current) {
+      formRef.current.reset();
+    }
+  };
+
   return (
-    <Form layout="vertical">
+    <Form layout="vertical" ref={formRef}>
       <Input
         layout="vertical"
         id="username"
         type="text"
         label="Username"
         defaultValue={user.username}
+        onChange={() => setEditMode(true)}
       />
       <Input
         layout="vertical"
@@ -23,8 +40,26 @@ const ChangeDetailsForm: React.FC<ChangeDetailsFormProps> = ({ user }) => {
         type="email"
         label="Email"
         defaultValue={user.email}
+        onChange={() => setEditMode(true)}
       />
-      <Input layout="vertical" id="password" type="password" label="Password" />
+      <div style={{ display: "flex", gap: "1rem", marginLeft: "auto" }}>
+        <Button
+          text="Save changes"
+          variant="primary"
+          type="submit"
+          size="medium"
+          disabled={!editMode}
+        />
+        {editMode && (
+          <Button
+            text="Cancel"
+            variant="secondary"
+            type="button"
+            size="medium"
+            onClick={() => handleCloseForm()}
+          />
+        )}
+      </div>
     </Form>
   );
 };
--- shared/components/ui/Form.tsx
+++ shared/components/ui/Form.tsx
@@ -7,13 +7,15 @@ interface FormProps {
   children: React.ReactNode;
   action?: (string | ((formData: FormData) => void)) | undefined;
   layout: "horizontal" | "vertical";
+  ref: React.Ref<HTMLFormElement>;
 }
 
-const Form: React.FC<FormProps> = ({ children, action, layout }) => {
+const Form: React.FC<FormProps> = ({ children, action, layout, ref }) => {
   return (
     <form
       action={action}
       className={`${styles.formContainer} ${styles[layout]}`}
+      ref={ref}
     >
       {children}
     </form>
