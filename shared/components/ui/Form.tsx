8b3ac6fe48eb623b06d30635bfaab76481b42933
Isuu1 <jakub.babelek@gmail.com>
2025-09-16 13:21:28 +0100

    Add optional to Form ref

--- shared/components/ui/Form.tsx
+++ shared/components/ui/Form.tsx
@@ -7,7 +7,7 @@ interface FormProps {
   children: React.ReactNode;
   action?: (string | ((formData: FormData) => void)) | undefined;
   layout: "horizontal" | "vertical";
-  ref: React.Ref<HTMLFormElement>;
+  ref?: React.Ref<HTMLFormElement>;
 }
 
 const Form: React.FC<FormProps> = ({ children, action, layout, ref }) => {
