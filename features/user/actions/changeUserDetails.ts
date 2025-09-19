d803bd33f8342ef87aae95031dbda0b8fdee10fa
Isuu1 <jakub.babelek@gmail.com>
2025-09-19 01:57:40 +0100

    Add basic action to change user details

--- features/user/actions/changeUserDetails.ts
+++ features/user/actions/changeUserDetails.ts
@@ -1,9 +1,36 @@
 "use server";
 
+import { createClient } from "@/supabase/server";
+
 export async function changeUserDetails(formData: FormData) {
-  const username = formData.get("username");
-  const email = formData.get("email");
+  const name = formData.get("name") as string;
+  const surname = formData.get("surname") as string;
+  const dob = formData.get("dob") as string;
+  const email = formData.get("email") as string;
+
+  try {
+    const supabase = await createClient();
+    const { data: user, error } = await supabase.auth.getUser();
+    if (error) {
+      console.error("Error fetching user:", error);
+      return;
+    }
+    console.log("Current user:", user);
+    const { data: userProfile, error: profileError } = await supabase
+      .from("profiles")
+      .select("*")
+      .eq("id", user?.user?.id)
+      .single();
+    if (profileError) {
+      console.error("Error fetching user profile:", profileError);
+      return;
+    }
+
+    console.log("User profile:", userProfile);
+  } catch (error) {
+    console.error("Error fetching user:", error);
+  }
 
   // Perform server-side logic to change user details
-  console.log("Changing user details to:", { username, email });
+  console.log("Changing user details to:", { name, surname, dob, email });
 }
