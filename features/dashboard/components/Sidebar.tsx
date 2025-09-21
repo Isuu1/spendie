9133f97ca70e1c90bcb61a08544680b8107ef5ca
Isuu1 <jakub.babelek@gmail.com>
2025-09-21 19:54:05 +0100

    Style DsahboardSidebar + add logout functionality

--- features/dashboard/components/Sidebar.tsx
+++ features/dashboard/components/Sidebar.tsx
@@ -10,46 +10,97 @@ import { TbLayoutDashboardFilled } from "react-icons/tb";
 import { IoWallet } from "react-icons/io5";
 import { FaCalculator } from "react-icons/fa6";
 import { FaRepeat } from "react-icons/fa6";
+import { IoSettings } from "react-icons/io5";
+import { FaSignOutAlt } from "react-icons/fa";
+import { createClient } from "@/supabase/client";
+import { useState } from "react";
+import ConfirmAction from "@/shared/components/ConfirmAction";
+import toast from "react-hot-toast";
+import { toastStyle } from "@/shared/styles/toastStyle";
 
 export default function Sidebar() {
+  const [signoutClicked, setSignoutClicked] = useState(false);
+
   const pathname = usePathname();
+
+  const supabase = createClient();
+
+  const handleSignOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error("Error signing out:", error);
+      toast.error("Error signing out. Please try again.", toastStyle);
+    }
+  };
 
   return (
     <div className={`${styles.sidebar}`}>
       <ul className={styles.menu}>
-        <Link href="/dashboard">
-          <li
+        <li>
+          <Link
+            href="/dashboard"
             className={`${styles.item} ${pathname === "/dashboard" ? styles.active : ""}`}
           >
             <TbLayoutDashboardFilled />
             <span className={styles.label}>Dashboard</span>
-          </li>
-        </Link>
-        <Link href="/transactions">
-          <li
+          </Link>
+        </li>
+
+        <li>
+          <Link
+            href="/transactions"
             className={`${styles.item} ${pathname === "/transactions" ? styles.active : ""}`}
           >
             <IoWallet />
             <span className={styles.label}>Transactions</span>
-          </li>
-        </Link>
-        <Link href="/budget-planner">
-          <li
+          </Link>
+        </li>
+
+        <li>
+          <Link
+            href="/budget-planner"
             className={`${styles.item} ${pathname === "/budget-planner" ? styles.active : ""}`}
           >
             <FaCalculator />
             <span className={styles.label}>Budget planner</span>
-          </li>
-        </Link>
-        <Link href="/recurring-payments">
-          <li
+          </Link>
+        </li>
+
+        <li>
+          <Link
+            href="/recurring-payments"
             className={`${styles.item} ${pathname === "/recurring-payments" ? styles.active : ""}`}
           >
             <FaRepeat />
             <span className={styles.label}>Recurring payments</span>
-          </li>
-        </Link>
+          </Link>
+        </li>
+
+        <li className={styles.settings}>
+          <Link
+            href="/user/account-settings"
+            className={`${styles.item} ${pathname === "/user/account-settings" ? styles.active : ""}`}
+          >
+            <IoSettings />
+            <span className={styles.label}>Settings</span>
+          </Link>
+        </li>
+
+        <li
+          className={`${styles.item} ${styles.logout}`}
+          onClick={() => setSignoutClicked(true)}
+        >
+          <FaSignOutAlt />
+          <span className={styles.label}>Logout</span>
+        </li>
       </ul>
+      {signoutClicked && (
+        <ConfirmAction
+          message="Are you sure you want to sign out?"
+          onCancel={() => setSignoutClicked(false)}
+          onConfirm={handleSignOut}
+        />
+      )}
     </div>
   );
 }
