4220d1d69d0437c0216c3b93ee95c276cec67cb7
Isuu1 <jakub.babelek@gmail.com>
2025-09-22 02:44:44 +0100

    Add UserProfileCard in Header

--- features/dashboard/components/DashboardHeader.tsx
+++ features/dashboard/components/DashboardHeader.tsx
@@ -1,11 +1,8 @@
 "use client";
 
-import Image from "next/image";
 import React, { useEffect, useState } from "react";
-import { AnimatePresence } from "motion/react";
 //Icons
 import { IoNotifications } from "react-icons/io5";
-import { RiArrowDownSLine } from "react-icons/ri";
 //Styles
 import styles from "./DashboardHeader.module.scss";
 //Types
@@ -13,11 +10,10 @@ import { UserProfile } from "@/features/user/types/user";
 //Utils
 import { getUserClient } from "@/features/user/api/getUserClient";
 //Components
-import UserModal from "@/features/user/components/UserModal";
+import UserProfileCard from "@/features/user/components/UserProfileCard";
 
 const DashboardHeader = () => {
   const [user, setUser] = useState<UserProfile | null>(null);
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     const fetchUser = async () => {
@@ -34,33 +30,7 @@ const DashboardHeader = () => {
   return (
     <>
       <div className={styles.header}>
-        <div
-          className={styles.userProfile}
-          onClick={() => setIsModalOpen(!isModalOpen)}
-        >
-          <AnimatePresence>
-            {isModalOpen && (
-              <UserModal
-                isOpen={isModalOpen}
-                onClose={() => setIsModalOpen(false)}
-              />
-            )}
-          </AnimatePresence>
-
-          <Image
-            className={styles.avatar}
-            src="https://i.pravatar.cc/150?img=3"
-            alt=""
-            width={32}
-            height={32}
-          />
-          <p className={styles.name}>
-            {user?.name} {user?.surname}
-          </p>
-          <div className={styles.icon}>
-            <RiArrowDownSLine />
-          </div>
-        </div>
+        <UserProfileCard user={user} />
         <i className={styles.notificationsIcon}>
           <IoNotifications />
           <span className={styles.counter}></span>
