34a6f97560cdb16e19f43773ee6ce5d1dfc0405c
Isuu1 <jakub.babelek@gmail.com>
2025-09-25 20:24:05 +0100

    Add type for tilesLibrary config

--- features/dashboard/config/tilesLibrary.ts
+++ features/dashboard/config/tilesLibrary.ts
@@ -3,7 +3,12 @@ import AccountsTile from "@/features/accounts/components/AccountsTile";
 import TotalBalanceTile from "@/features/total-balance/components/TotalBalanceTile";
 import TransactionsTile from "@/features/transactions/components/TransactionsTile";
 
-export const tilesLibrary = [
+export type TileDefinition = {
+  name: "Total Balance" | "Accounts" | "Recent transactions"; // literal union
+  component: React.ComponentType;
+};
+
+export const tilesLibrary: TileDefinition[] = [
   {
     name: "Total Balance",
     component: TotalBalanceTile,
