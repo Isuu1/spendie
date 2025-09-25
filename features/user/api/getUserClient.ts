7068113f358f70a7b8b98d348629eb98c6300276
Isuu1 <jakub.babelek@gmail.com>
2025-09-25 20:32:59 +0100

    Improve fetchUser function for client and server

--- features/user/api/getUserClient.ts
+++ features/user/api/getUserClient.ts
@@ -1,23 +1,7 @@
 import { createClient } from "@/supabase/client";
+import { fetchUser } from "../lib/fetchUser";
 
 export async function getUserClient() {
   const supabase = createClient();
-
-  const { data: authUser, error: authError } = await supabase.auth.getUser();
-  if (authError || !authUser?.user) {
-    console.error("Error fetching user:", authError);
-    return { error: authError, user: null };
-  }
-
-  const { data: userProfile, error: profileError } = await supabase
-    .from("profiles")
-    .select("*")
-    .eq("id", authUser?.user?.id)
-    .single();
-  if (profileError) {
-    console.error("Error fetching user profile:", profileError);
-    return { error: profileError, user: null };
-  }
-
-  return { error: null, user: userProfile };
+  return fetchUser(supabase);
 }
