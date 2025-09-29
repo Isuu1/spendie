b1d92f76553d4c08ababdb3f73939f74f76d6ca4
Isuu1 <jakub.babelek@gmail.com>
2025-09-29 19:06:31 +0100

    Cleanup in Accounts panel

--- features/accounts/components/AccountsTile.tsx
+++ features/accounts/components/AccountsTile.tsx
@@ -6,12 +6,11 @@ import styles from "./AccountsTile.module.scss";
 import { getAccountsServer } from "@/features/accounts/api/server";
 //Types
 import { Account } from "@/shared/types/account";
+//Components
 import AccountsList from "./AccountsList";
 
 const AccountsTile = async () => {
   const accounts: Account[] = (await getAccountsServer()) ?? [];
-
-  console.log("Accounts:", accounts);
 
   if (accounts.length === 0) {
     return (
@@ -26,23 +25,6 @@ const AccountsTile = async () => {
   }
 
   return (
-    // <div className={styles.accountsTile}>
-    //   {accounts.map((account) => (
-    //     <div key={account.account_id} className={styles.account}>
-    //       <div className={styles.details}>
-    //         <h4>{account.official_name}</h4>
-    //         <p className={styles.type}>{account.subtype}</p>
-    //         <p>**** **** **** {account.mask}</p>
-    //       </div>
-    //       {/* <em className={styles.number}>****** {account.mask}</em> */}
-    //       <p className={styles.balance}>
-    //         <span>{account.balances.iso_currency_code}</span>
-    //         <span>{account.balances.current}</span>
-    //       </p>
-    //       {/* <div className={styles.bgcover}></div> */}
-    //     </div>
-    //   ))}
-    // </div>
     <div className={styles.accountsTile}>
       <AccountsList accounts={accounts} />
     </div>
