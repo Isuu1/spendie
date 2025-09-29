8762dd6db41948fe139c00eca3ec0dbb33b40674
Isuu1 <jakub.babelek@gmail.com>
2025-09-29 11:32:53 +0100

    Login form - replace username -> email

--- features/auth/components/LoginForm.tsx
+++ features/auth/components/LoginForm.tsx
@@ -17,8 +17,8 @@ const LoginForm = () => {
   return (
     <form action={formAction}>
       <div>
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" />
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" />
       </div>
       <div>
         <label htmlFor="password">Password</label>
