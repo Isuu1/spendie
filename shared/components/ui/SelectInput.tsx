c66f628f16bda1db3dc39ddaea42bcc84637508a
Isuu1 <jakub.babelek@gmail.com>
2025-10-19 18:57:20 +0100

    Improve SelectInput dropdown animation

--- shared/components/ui/SelectInput.tsx
+++ shared/components/ui/SelectInput.tsx
@@ -15,9 +15,9 @@ interface SelectInputProps {
 }
 
 const selectModeOptionsVariants = {
-  hidden: { opacity: 0, y: -10 },
-  visible: { opacity: 1, y: 0 },
-  exit: { opacity: 0, y: -10 },
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+  exit: { opacity: 0 },
   transition: { duration: 0.15 },
 };
 
