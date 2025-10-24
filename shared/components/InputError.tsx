6a835b9582e794d658e041e6675d78edf987f1d0
Isuu1 <jakub.babelek@gmail.com>
2025-10-24 14:53:49 +0100

    Minor styling change + animation in InputError

--- shared/components/InputError.tsx
+++ shared/components/InputError.tsx
@@ -1,5 +1,9 @@
+//Icons
 import { MdOutlineError } from "react-icons/md";
+//Styles
 import styles from "./InputError.module.scss";
+//Animations
+import { motion } from "motion/react";
 
 interface InputErrorProps {
   errors?: string[];
@@ -9,13 +13,17 @@ export default function InputError({ errors = [] }: InputErrorProps) {
   if (errors.length === 0) return null;
 
   return (
-    <div className={styles.errorContainer}>
+    <motion.div
+      className={styles.errorContainer}
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+    >
       {errors.map((err, index) => (
         <span key={index} className={styles.errorMessage}>
           <MdOutlineError className={styles.icon} />
           {err}
         </span>
       ))}
-    </div>
+    </motion.div>
   );
 }
