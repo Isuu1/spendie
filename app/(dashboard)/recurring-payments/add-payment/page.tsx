9aedc9c2518c19d60c8a44fd396c646bfb70a39a
Isuu1 <jakub.babelek@gmail.com>
2025-10-25 18:15:59 +0100

    Add BackButton + page global class

--- app/(dashboard)/recurring-payments/add-payment/page.tsx
+++ app/(dashboard)/recurring-payments/add-payment/page.tsx
@@ -1,8 +1,10 @@
 import AddPaymentForm from "@/features/recurring-payments/components/AddPaymentForm";
+import BackButton from "@/shared/components/BackButton";
 
 export default function Page() {
   return (
     <div className="page">
+      <BackButton />
       <h2>Add new recurring payment</h2>
       <AddPaymentForm />
     </div>
