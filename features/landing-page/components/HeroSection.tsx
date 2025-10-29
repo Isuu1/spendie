710a7db945992eb14e6ae0cb6673535b23c28270
Isuu1 <jakub.babelek@gmail.com>
2025-10-29 23:50:14 +0000

    Add hero section content and image

--- features/landing-page/components/HeroSection.tsx
+++ features/landing-page/components/HeroSection.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import Image from "next/image";
+//Styles
+import styles from "./HeroSection.module.scss";
+//Images
+import dashboard from "@/public/images/dashboard2.png";
 
 const HeroSection = () => {
   return (
-    <div>
-      <h1>Hero section</h1>
+    <div className={styles.heroSection}>
+      <div className={styles.headline}>
+        <h1>Take control of your money — effortlessly.</h1>
+        <p className={styles.subheadline}>
+          Spendie brings your accounts, bills, and recurring payments together
+          into one clear dashboard — so you can plan smarter and stress less.
+        </p>
+      </div>
+      <div className={styles.image}>
+        <Image src={dashboard} alt="Hero Image" />
+      </div>
     </div>
   );
 };
