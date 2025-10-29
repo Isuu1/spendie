1f17804b7a9e228726d83fe6e2c3229e3f117b5d
Isuu1 <jakub.babelek@gmail.com>
2025-10-29 17:18:50 +0000

    Add Header mobile menu + animation

--- features/landing-page/components/Header.tsx
+++ features/landing-page/components/Header.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+"use client";
+
+import React, { useRef, useState } from "react";
+import Link from "next/link";
 //Styles
 import styles from "./Header.module.scss";
+//Icons
 import { TiThMenu } from "react-icons/ti";
-import Link from "next/link";
+//Hooks
+import { useClickOutside } from "@/shared/hooks/useClickOutside";
+//Animations
+import { motion, AnimatePresence } from "motion/react";
+
+const mobileMenuVariants = {
+  hidden: { x: 100 },
+  visible: { x: 0 },
+  exit: { x: 120 },
+};
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const mobileMenuRef = useRef<HTMLUListElement>(null);
+
+  useClickOutside(mobileMenuRef, () => setMenuOpen(false));
+
   return (
     <div className={styles.header}>
       <h2>Spendie.</h2>
@@ -17,11 +36,31 @@ const Header = () => {
       <Link className={styles.loginButton} href="/login">
         Login
       </Link>
-      <div className={styles.mobileNav}>
+      <div className={styles.mobileNav} onClick={() => setMenuOpen(!menuOpen)}>
         <i className={styles.icon}>
           <TiThMenu />
         </i>
       </div>
+      <AnimatePresence>
+        {menuOpen && (
+          <motion.ul
+            className={styles.mobileMenu}
+            ref={mobileMenuRef}
+            variants={mobileMenuVariants}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            transition={{ duration: 0.2, type: "tween" }}
+          >
+            <li className={styles.mobileMenuItem}>Home</li>
+            <li className={styles.mobileMenuItem}>About</li>
+            <li className={styles.mobileMenuItem}>Contact</li>
+            <Link href="/login">
+              <li className={styles.mobileMenuItem}>Login</li>
+            </Link>
+          </motion.ul>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
