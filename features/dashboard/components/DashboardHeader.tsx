d66ccb02ac4ba3443143b42b40e06000fc5130f2
Isuu1 <jakub.babelek@gmail.com>
2025-11-10 21:12:17 +0000

    Add DashboardOptions + move panel cotrol into it + styling

--- features/dashboard/components/DashboardHeader.tsx
+++ features/dashboard/components/DashboardHeader.tsx
@@ -7,19 +7,14 @@ import styles from "./DashboardHeader.module.scss";
 import { getUserServer } from "@/features/user/api/getUserServer";
 //Components
 import UserProfileCard from "@/features/user/components/UserProfileCard";
-import DashboardPanelsControls from "./DashboardPanelsControls";
-//Api
-import { getUserSettingsServer } from "@/features/user/api/getUserSettingsServer";
 
 const DashboardHeader = async () => {
   const { user, error: userError } = await getUserServer();
-  const { settings, error: settingsError } = await getUserSettingsServer();
 
   return (
     <>
       <div className={styles.header}>
         {user && !userError && <UserProfileCard user={user} />}
-        {settings && !settingsError && <DashboardPanelsControls />}
         <i className={styles.notificationsIcon}>
           <IoNotifications />
           <span className={styles.counter}></span>
---
+++ features/dashboard/components/DashboardPanelsMenu.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import React, { useRef } from "react";
+//Animations
+import { motion } from "motion/react";
+import { useUserSettingsClient } from "@/features/user/api/useUserSettingsClient";
+import { useTogglePanelVisibility } from "@/features/user/api/useTogglePanelVisibility";
+import { PanelName, panelsLibrary } from "../config/panelsLibrary";
+//Styles
+import styles from "./DashboardPanelsMenu.module.scss";
+import { useClickOutside } from "@/shared/hooks/useClickOutside";
+
+const panelMenuVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+  exit: { opacity: 0 },
+  transition: { duration: 0.15 },
+};
+
+interface DashboardPanelsMenuProps {
+  onClose: () => void;
+}
+
+const DashboardPanelsMenu: React.FC<DashboardPanelsMenuProps> = ({
+  onClose,
+}) => {
+  const { data: settings } = useUserSettingsClient();
+
+  const { mutate: togglePanel } = useTogglePanelVisibility();
+
+  const visiblePanels = settings?.visible_panels || [];
+
+  const panelMenuRef = useRef<HTMLUListElement>(null);
+
+  const isPanelActive = (panelName: PanelName) => {
+    return visiblePanels.includes(panelName);
+  };
+
+  const handleChange = (panelName: PanelName, isActive: boolean) => {
+    togglePanel({ panelName, isActive });
+  };
+
+  useClickOutside(panelMenuRef, () => onClose());
+
+  return (
+    <motion.ul
+      ref={panelMenuRef}
+      className={styles.menu}
+      variants={panelMenuVariants}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+    >
+      {panelsLibrary.map((panel) => (
+        <li className={styles.menuItem} key={panel.name}>
+          <span
+            className={`${styles.panelStatus} ${
+              isPanelActive(panel.name) ? styles.active : styles.inactive
+            }`}
+            onClick={() =>
+              handleChange(panel.name, !!isPanelActive(panel.name))
+            }
+          >
+            <motion.span
+              className={styles.indicator}
+              initial={false}
+              animate={{
+                x: isPanelActive(panel.name) ? "0%" : "100%",
+              }}
+            />
+          </span>
+          <span className={styles.panelName}>{panel.name}</span>
+        </li>
+      ))}
+    </motion.ul>
+  );
+};
+
+export default DashboardPanelsMenu;
