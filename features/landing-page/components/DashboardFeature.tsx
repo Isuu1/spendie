df5348b32006d031743d137ba4874ab285f449af
Isuu1 <jakub.babelek@gmail.com>
2025-11-15 15:48:00 +0000

    Add DashboardFeature content

--- features/landing-page/components/DashboardFeature.tsx
+++ features/landing-page/components/DashboardFeature.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import dashboard from "@/public/images/dashboard-feature.png";
+import Image from "next/image";
+//Styles
+import styles from "./DashboardFeature.module.scss";
+import Headline from "./Headline";
 
 const DashboardFeature = () => {
   return (
-    <div>
-      <h1>Dashboard Feature</h1>
+    <div className={styles.dashboardFeature}>
+      <Headline
+        title="Choose Your Dashboard"
+        subtitle="Customize your experience with our advanced dashboard features."
+      />
+      <div className={styles.imageWrapper}>
+        <Image
+          src={dashboard}
+          alt="Advanced Payments Illustration"
+          className={styles.image}
+          fill
+          unoptimized
+          priority
+        />
+      </div>
     </div>
   );
 };
