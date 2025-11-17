c1e59f3ab9d7ea037e9594b4873f044bdfe214d7
Isuu1 <jakub.babelek@gmail.com>
2025-11-17 18:24:39 +0000

    Style auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,8 +3,11 @@ import AuthNav from "@/features/auth/components/AuthNav";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="auth-layout">
-      <AuthNav />
-      {children}
+      <div className="inner-wrapper">
+        <AuthNav />
+        {children}
+      </div>
+      <div className="image"></div>
     </div>
   );
 }
