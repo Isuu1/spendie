a90428d657159416aac0008ca02e00aed3c8b211
Isuu1 <jakub.babelek@gmail.com>
2025-11-18 17:05:42 +0000

    Add SignupForm skeleton

--- features/auth/components/SignupForm.tsx
+++ features/auth/components/SignupForm.tsx
@@ -1,7 +1,23 @@
 "use client";
-import React, { useActionState } from "react";
+import React, { useActionState, useState } from "react";
+//Types
 import { SignupFormState } from "../types/forms";
+//Actions
 import { signup } from "../lib/actions/signup";
+//Styles
+import styles from "./SignupForm.module.scss";
+//Components
+import Button from "@/shared/components/ui/Button";
+import Providers from "./Providers";
+import Form from "@/shared/components/ui/Form";
+import Input from "@/shared/components/ui/Input";
+//Icons
+import { IoSend } from "react-icons/io5";
+import { MdEmail } from "react-icons/md";
+import { RiLockPasswordFill } from "react-icons/ri";
+import { FaUserAlt } from "react-icons/fa";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
 
 const initialState: SignupFormState = {
   error: null,
@@ -12,28 +28,61 @@ const initialState: SignupFormState = {
 };
 
 const SignupForm = () => {
-  const [state, formAction] = useActionState(signup, initialState);
+  const [state, formAction, isPending] = useActionState(signup, initialState);
+  const [showPassword, setShowPassword] = useState(false);
   console.log("LoginForm state", state);
   return (
-    <form action={formAction}>
-      <div>
-        <label htmlFor="username">Email</label>
-        <input type="text" id="email" name="email" />
-      </div>
-      <div>
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" />
-      </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" />
-      </div>
-      <div>
-        <label htmlFor="password">Confirm password</label>
-        <input type="password" id="confirmPassword" name="confirmPassword" />
-      </div>
-      <button type="submit">Signup</button>
-    </form>
+    <div className={styles.signupForm}>
+      <h1 className={styles.logo}>Spendie.</h1>
+      <h3 className={styles.subtitle}>
+        Manage your finances, track payments, and pick up right where you left
+        off.
+      </h3>
+      <Form layout="vertical" action={formAction}>
+        <Input label="Email" id="email" type="text" icon={<MdEmail />} />
+        <Input
+          label="Username"
+          id="username"
+          type="text"
+          icon={<FaUserAlt />}
+        />
+        <Input
+          label="Password"
+          id="password"
+          type={showPassword ? "text" : "password"}
+          icon={<RiLockPasswordFill />}
+          passwordIcon={
+            showPassword ? (
+              <FaEyeSlash
+                onClick={() => setShowPassword(false)}
+                style={{ cursor: "pointer" }}
+              />
+            ) : (
+              <FaEye
+                onClick={() => setShowPassword(true)}
+                style={{ cursor: "pointer" }}
+              />
+            )
+          }
+        />
+        <Input
+          label="Confirm Password"
+          id="confirmPassword"
+          type={showPassword ? "text" : "password"}
+          icon={<RiLockPasswordFill />}
+        />
+        <Button
+          className={styles.loginButton}
+          text={isPending ? "Creating account..." : "Create account"}
+          size="large"
+          variant="primary"
+          type="submit"
+          icon={<IoSend />}
+          disabled={isPending}
+        />
+        <Providers />
+      </Form>
+    </div>
   );
 };
 
