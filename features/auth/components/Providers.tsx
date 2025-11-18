7ba1cbc28f13a9755e6bc7d8734f5832cfdab4d2
Isuu1 <jakub.babelek@gmail.com>
2025-11-18 17:14:33 +0000

    Style providers

--- features/auth/components/Providers.tsx
+++ features/auth/components/Providers.tsx
@@ -4,31 +4,19 @@ import styles from "./Providers.module.scss";
 //Icons
 import { FcGoogle } from "react-icons/fc";
 import { BsApple } from "react-icons/bs";
-//Components
-import Button from "@/shared/components/ui/Button";
 
 const Providers = () => {
   return (
     <div className={styles.providersContainer}>
       <p className={styles.loginOptions}>OR</p>
-      <Button
-        className={styles.googleLoginButton}
-        text="Login with Google"
-        size="large"
-        variant="secondary"
-        type="button"
-        icon={<FcGoogle />}
-        iconPosition="left"
-      />
-      <Button
-        className={styles.appleLoginButton}
-        text="Login with Apple"
-        size="large"
-        variant="secondary"
-        type="button"
-        icon={<BsApple />}
-        iconPosition="left"
-      />
+      <div className={styles.providers}>
+        <div className={styles.provider}>
+          <FcGoogle />
+        </div>
+        <div className={styles.provider}>
+          <BsApple />
+        </div>
+      </div>
     </div>
   );
 };
