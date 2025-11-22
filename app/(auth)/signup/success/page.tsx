7622f14c0cf61c1b711aba2e39b681d32bfb06b9
Isuu1 <jakub.babelek@gmail.com>
2025-11-22 22:11:25 +0000

    Fix deployment issue

--- app/(auth)/signup/success/page.tsx
+++ app/(auth)/signup/success/page.tsx
@@ -3,6 +3,8 @@
 import SignupSuccess from "@/features/auth/components/SignupSuccess";
 import { useSearchParams } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   const params = useSearchParams();
   const email = params.get("email");
