8786fc873e176fb08347915bb23f659ae20d1a1e
Isuu1 <jakub.babelek@gmail.com>
2025-11-23 19:48:53 +0000

    Cleanup

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,3 @@
-// import { type NextRequest } from "next/server";
-// import { updateSession } from "@/supabase/middleware";
-
-// export async function middleware(request: NextRequest) {
-//   return await updateSession(request);
-// }
-
-// export const config = {
-//   matcher: [
-//     // Run middleware on all routes except:
-//     // '/', '/login', 'signup', 'auth' and Next.js/static asset routes
-//     "/((?!api/auth|_next|favicon.ico|login|signup|auth|$).*)",
-//   ],
-// };
 import { NextResponse, type NextRequest } from "next/server";
 import { updateSession } from "@/supabase/middleware";
 
