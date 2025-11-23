c01eed013fea7ee98f6f4c2748d75d1b4d1f455a
Isuu1 <jakub.babelek@gmail.com>
2025-11-23 19:31:26 +0000

    Add logged in user redirect on home page + update middleware + refactor user Api

--- features/user/api/getUserServer.ts
+++ features/user/api/getUserServer.ts
@@ -1,7 +1,22 @@
 import { createClient } from "@/supabase/server";
-import { fetchUser } from "../lib/fetchUser";
 
 export async function getUserServer() {
   const supabase = await createClient();
-  return fetchUser(supabase);
+  const { data: authUser, error: authError } = await supabase.auth.getUser();
+  if (authError || !authUser?.user) {
+    console.error("Error fetching user:", authError);
+    return { error: authError, user: null };
+  }
+
+  const { data: userProfile, error: profileError } = await supabase
+    .from("profiles")
+    .select("*")
+    .eq("id", authUser?.user?.id)
+    .single();
+  if (profileError) {
+    console.error("Error fetching user profile:", profileError);
+    return { error: profileError, user: null };
+  }
+
+  return { error: null, user: userProfile };
 }
