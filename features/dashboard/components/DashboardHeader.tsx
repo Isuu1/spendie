e3cd4b7f9484cba562670871238f837cec7d37fa
Isuu1 <jakub.babelek@gmail.com>
2025-11-29 17:28:04 +0000

    Add menu icon for mobile view

--- features/dashboard/components/DashboardHeader.tsx
+++ features/dashboard/components/DashboardHeader.tsx
@@ -3,16 +3,21 @@
 import React from "react";
 //Icons
 import { IoNotifications } from "react-icons/io5";
+import { TiThMenu } from "react-icons/ti";
 //Styles
 import styles from "./DashboardHeader.module.scss";
 //Components
 import UserProfileCard from "@/features/user/components/UserProfileCard";
+import clsx from "clsx";
 
 const DashboardHeader = () => {
   return (
     <>
       <div className={styles.header}>
-        <i className={styles.notificationsIcon}>
+        <i className={clsx(styles.icon, styles.menuIcon)}>
+          <TiThMenu />
+        </i>
+        <i className={clsx(styles.icon, styles.notificationsIcon)}>
           <IoNotifications />
           <span className={styles.counter}></span>
         </i>
