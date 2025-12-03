e14cb0f4c0bbb5b334f94a5b6fd709cc0ab5bfd3
Isuu1 <jakub.babelek@gmail.com>
2025-12-03 19:15:55 +0000

    Style accounts panel buttons

--- features/accounts/components/AccountsList.tsx
+++ features/accounts/components/AccountsList.tsx
@@ -1,15 +1,17 @@
 "use client";
 
-//import React, { useCallback, useEffect, useState } from "react";
-//import useEmblaCarousel from "embla-carousel-react";
+import React, { useCallback, useEffect, useState } from "react";
+import useEmblaCarousel from "embla-carousel-react";
 //Styles
 import styles from "./AccountsList.module.scss";
-//import { EmblaCarouselType } from "embla-carousel";
+import { EmblaCarouselType } from "embla-carousel";
 //Icons
-//import { FaArrowRight } from "react-icons/fa";
-//import { FaArrowLeft } from "react-icons/fa";
+import { IoIosArrowForward } from "react-icons/io";
+
+import { IoIosArrowBack } from "react-icons/io";
+
 //Utils
-//import { generateAccountBackground } from "../utils/generateAccountBackground";
+import { generateAccountBackground } from "../utils/generateAccountBackground";
 //Types
 import { Account } from "@/features/accounts/types/account";
 //Hooks
@@ -23,30 +25,30 @@ interface AccountsListProps {
 
 const AccountsList: React.FC<AccountsListProps> = ({ accounts }) => {
   const { data: user, error } = useUserClient();
-  // const [emblaRef, emblaApi] = useEmblaCarousel();
+  const [emblaRef, emblaApi] = useEmblaCarousel();
 
-  // const [prevBtnDisabled, setPrevBtnDisabled] = useState(false);
-  // const [nextBtnDisabled, setNextBtnDisabled] = useState(false);
+  const [prevBtnDisabled, setPrevBtnDisabled] = useState(false);
+  const [nextBtnDisabled, setNextBtnDisabled] = useState(false);
 
-  // const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
-  //   setPrevBtnDisabled(!emblaApi.canScrollPrev());
-  //   setNextBtnDisabled(!emblaApi.canScrollNext());
-  // }, []);
+  const onSelect = useCallback((emblaApi: EmblaCarouselType) => {
+    setPrevBtnDisabled(!emblaApi.canScrollPrev());
+    setNextBtnDisabled(!emblaApi.canScrollNext());
+  }, []);
 
-  // useEffect(() => {
-  //   if (!emblaApi) return;
+  useEffect(() => {
+    if (!emblaApi) return;
 
-  //   onSelect(emblaApi);
-  //   emblaApi.on("reInit", onSelect).on("select", onSelect);
-  // }, [emblaApi, onSelect]);
+    onSelect(emblaApi);
+    emblaApi.on("reInit", onSelect).on("select", onSelect);
+  }, [emblaApi, onSelect]);
 
-  // const scrollPrev = useCallback(() => {
-  //   if (emblaApi) emblaApi.scrollPrev();
-  // }, [emblaApi]);
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
 
-  // const scrollNext = useCallback(() => {
-  //   if (emblaApi) emblaApi.scrollNext();
-  // }, [emblaApi]);
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
 
   if (error) {
     console.error("Error fetching user data:", error);
@@ -61,51 +63,51 @@ const AccountsList: React.FC<AccountsListProps> = ({ accounts }) => {
     );
 
   return (
-    // <div ref={emblaRef} className={styles.emblaContainer}>
-    <div className={styles.accounts}>
-      {accounts.map((account) => (
-        <div
-          key={account.account_id}
-          className={`${styles.account}`}
-          // style={{
-          //   background: generateAccountBackground(account.subtype ?? ""),
-          // }}
-        >
-          <div className={styles.innerWrapper}>
-            <div className={styles.details}>
-              <h4>{account.official_name}</h4>
-              <p className={styles.type}>{account.subtype}</p>
-              <p>**** **** **** {account.mask}</p>
+    <div ref={emblaRef} className={styles.emblaContainer}>
+      <div className={styles.accounts}>
+        {accounts.map((account) => (
+          <div
+            key={account.account_id}
+            className={`${styles.account}`}
+            style={{
+              background: generateAccountBackground(account.subtype ?? ""),
+            }}
+          >
+            <div className={styles.innerWrapper}>
+              <div className={styles.details}>
+                <h4>{account.official_name}</h4>
+                <p className={styles.type}>{account.subtype}</p>
+                <p>**** **** **** {account.mask}</p>
+              </div>
+              <p className={styles.balance}>
+                <span>Balance</span>
+                <span>£{account.balances.current}</span>
+              </p>
             </div>
-            <p className={styles.balance}>
-              <span>Balance</span>
-              <span>£{account.balances.current}</span>
-            </p>
-          </div>
 
-          <div className={styles.shape}></div>
-        </div>
-      ))}
+            <div className={styles.shape}></div>
+          </div>
+        ))}
+      </div>
+      <div
+        className={`${styles.buttons}
+    ${!prevBtnDisabled ? `${styles.blur} ${styles.left}` : ""}
+    ${!nextBtnDisabled ? `${styles.blur} ${styles.right}` : ""}`}
+      >
+        <button
+          className={`${prevBtnDisabled ? styles.disabled : ""} ${styles.button}`}
+          onClick={scrollPrev}
+        >
+          <IoIosArrowBack />
+        </button>
+        <button
+          className={`${nextBtnDisabled ? styles.disabled : ""} ${styles.button}`}
+          onClick={scrollNext}
+        >
+          <IoIosArrowForward />
+        </button>
+      </div>
     </div>
-    //   <div
-    //     className={`${styles.buttons}
-    // ${!prevBtnDisabled ? `${styles.blur} ${styles.left}` : ""}
-    // ${!nextBtnDisabled ? `${styles.blur} ${styles.right}` : ""}`}
-    //   >
-    //     <button
-    //       className={`${prevBtnDisabled ? styles.disabled : ""} ${styles.button}`}
-    //       onClick={scrollPrev}
-    //     >
-    //       <FaArrowLeft />
-    //     </button>
-    //     <button
-    //       className={`${nextBtnDisabled ? styles.disabled : ""} ${styles.button}`}
-    //       onClick={scrollNext}
-    //     >
-    //       <FaArrowRight />
-    //     </button>
-    //   </div>
-    // </div>
   );
 };
 
