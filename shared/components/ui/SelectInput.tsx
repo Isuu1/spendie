cd32b07daae89659888fc139f97de02469175b25
Isuu1 <jakub.babelek@gmail.com>
2026-04-16 11:08:06 +0100

    Add input field wrapper to SelectInput

--- shared/components/ui/SelectInput.tsx
+++ shared/components/ui/SelectInput.tsx
@@ -8,6 +8,7 @@ import { useClickOutside } from "@/shared/hooks/useClickOutside";
 import { TbArrowBigDownLineFilled } from "react-icons/tb";
 //Components
 import PopUp from "../PopUp";
+import InputFieldWrapper from "../InputFieldWrapper";
 
 type Option = {
   label: string;
@@ -55,7 +56,7 @@ const SelectInput: React.FC<SelectInputProps> = ({
   useClickOutside(selectRef, () => setShowOptions(false));
 
   return (
-    <div className={`${styles.inputContainer} ${!label ? styles.noLabel : ""}`}>
+    <InputFieldWrapper id={id} label={label}>
       {/* Hidden input to store the selected value */}
       <input
         id={id}
@@ -64,57 +65,47 @@ const SelectInput: React.FC<SelectInputProps> = ({
         value={value?.value || ""}
         className={styles.selectInput}
       />
-
-      {label && (
-        <label htmlFor={id} className={styles.label}>
-          {label}
-        </label>
-      )}
-      <div className={styles.fieldWrapper}>
-        <div className={styles.inputFieldWrapper}>
-          <span
-            className={`${styles.inputField} ${styles.selectInputField} ${icon ? styles.withIcon : ""}`}
-            onClick={() => setShowOptions((prev) => !prev)}
-            role="button"
-          >
-            {icon && <i className={styles.icon}>{icon}</i>}
-            {value?.label ?? selectOptions[0]?.label}
-            <motion.i
-              className={styles.dropdownIcon}
-              animate={{ rotate: showOptions ? 180 : 0 }}
-              transition={{ duration: 0.1 }}
+      <span
+        className={`${styles.inputField} ${styles.selectInputField} ${icon ? styles.withIcon : ""}`}
+        onClick={() => setShowOptions((prev) => !prev)}
+        role="button"
+      >
+        {icon && <i className={styles.icon}>{icon}</i>}
+        {value?.label ?? selectOptions[0]?.label}
+        <motion.i
+          className={styles.dropdownIcon}
+          animate={{ rotate: showOptions ? 180 : 0 }}
+          transition={{ duration: 0.1 }}
+        >
+          <TbArrowBigDownLineFilled />
+        </motion.i>
+      </span>
+      <AnimatePresence>
+        {showOptions && (
+          <PopUp popupRef={selectRef} top={40} left={0} width="100%">
+            <ul
+              style={{
+                gridTemplateColumns: `repeat(${columns}, minmax(0, 1fr))`,
+              }}
+              className={styles.selectOptions}
             >
-              <TbArrowBigDownLineFilled />
-            </motion.i>
-          </span>
-        </div>
-        <AnimatePresence>
-          {showOptions && (
-            <PopUp popupRef={selectRef} top={40} left={0} width="100%">
-              <ul
-                style={{
-                  gridTemplateColumns: `repeat(${columns}, minmax(0, 1fr))`,
-                }}
-                className={styles.selectOptions}
-              >
-                {optionsHeader && (
-                  <li className={styles.optionsHeader}>{optionsHeader}</li>
-                )}
-                {selectOptions?.map((option) => (
-                  <li
-                    key={option.value}
-                    onClick={() => handleOptionClick(option)}
-                    className={`${styles.option} ${value?.value === option.value ? styles.active : ""}`}
-                  >
-                    {option.label}
-                  </li>
-                ))}
-              </ul>
-            </PopUp>
-          )}
-        </AnimatePresence>
-      </div>
-    </div>
+              {optionsHeader && (
+                <li className={styles.optionsHeader}>{optionsHeader}</li>
+              )}
+              {selectOptions?.map((option) => (
+                <li
+                  key={option.value}
+                  onClick={() => handleOptionClick(option)}
+                  className={`${styles.option} ${value?.value === option.value ? styles.active : ""}`}
+                >
+                  {option.label}
+                </li>
+              ))}
+            </ul>
+          </PopUp>
+        )}
+      </AnimatePresence>
+    </InputFieldWrapper>
   );
 };
 
