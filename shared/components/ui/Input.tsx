566c84dc4606a19fac44fa3de620c6f6526511f5
Isuu1 <jakub.babelek@gmail.com>
2026-04-28 18:41:53 +0100

    Add more shadcn components + style input

--- shared/components/ui/Input.tsx
+++ shared/components/ui/Input.tsx
@@ -13,23 +13,30 @@ type InputProps = React.ComponentProps<typeof ShadcnInput> & {
   errors?: string[];
   icon?: React.ReactNode;
   passwordIcon?: React.ReactNode;
+  value?: string | number | Date;
 };
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ icon, passwordIcon, id, label, errors, className, ...props }, ref) => {
+  (
+    { icon, passwordIcon, id, label, errors, className, value, ...props },
+    ref,
+  ) => {
     return (
       <InputFieldWrapper id={id} label={label} errors={errors}>
         <ShadcnInput
-          className={cn(icon && "pl-10", className)}
+          className={cn(
+            "bg-bg-surface-dark rounded-md px-4 py-2 border-0 transition-all outline-3 outline-transparent duration-200 ease-in-out flex flex-1 items-center",
+            "focus:outline-brand focus:outline-3",
+            icon && "pl-10",
+            className,
+          )}
           ref={ref}
           id={id}
           name={id}
           {...props}
           value={
-              value instanceof Date
-                ? value.toISOString().slice(0, 10)
-                : value
-            }
+            value instanceof Date ? value.toISOString().slice(0, 10) : value
+          }
         />
         {icon && <span className="absolute left-3 text-base!">{icon}</span>}
         {passwordIcon && (
