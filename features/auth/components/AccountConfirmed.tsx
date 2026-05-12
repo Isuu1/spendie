f9ea939e52b4f782241763cc60babe5519110bd8
Isuu1 <jakub.babelek@gmail.com>
2026-05-12 10:29:49 +0100

    Remove stylesheet from AccountConfirmed

--- features/auth/components/AccountConfirmed.tsx
+++ features/auth/components/AccountConfirmed.tsx
@@ -1,23 +1,21 @@
 import React from "react";
-//Styles
-import styles from "./AccountConfirmed.module.scss";
 import Link from "next/link";
 import success from "@/public/images/success.svg";
 import Image from "next/image";
 
 const AccountConfirmed = () => {
   return (
-    <div className={styles.accountConfirmed}>
+    <div className="flex flex-col items-center justify-center gap-6 text-center p-8">
       <Image
         src={success}
         alt="Account Confirmed"
-        className={styles.successImage}
+        className="relative! w-26!"
         fill
       />
       <h2>Your account is verified!</h2>
       <p>You can now log in and start using Spendie.</p>
 
-      <Link href="/login" className={styles.loginButton}>
+      <Link href="/login" className="bg-brand rounded-md px-4 py-2">
         Go to Login
       </Link>
     </div>
