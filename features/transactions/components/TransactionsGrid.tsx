fa3ed54e18a2ec0cd1a3a869d2d3d9271667aaa5
Isuu1 <jakub.babelek@gmail.com>
2026-06-10 00:00:03 +0100

    Add table in TransactionsGrid

--- features/transactions/components/TransactionsGrid.tsx
+++ features/transactions/components/TransactionsGrid.tsx
@@ -2,11 +2,140 @@
 
 import React from "react";
 import { useTransactions } from "../hooks/useTransactions";
+import { Transaction } from "../types/transaction";
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  getPaginationRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Button from "@/shared/components/ui/Button";
+import { displayTransactionCategory } from "../lib/utils";
+import Image from "next/image";
+
+const columns: ColumnDef<Transaction>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+    cell: ({ getValue, row }) => {
+      const name = getValue() as string;
+      const category = row.original.category as string;
+      const imageUrl = row.original.image_url as string;
+      return (
+        <div className="flex items-center gap-3">
+          <Image
+            src={imageUrl}
+            alt="Transaction"
+            width={40}
+            height={40}
+            className="rounded-full"
+          />
+          <div className="flex flex-col items-start gap-1">
+            <p>{displayTransactionCategory(category)}</p>
+            <p>{name}</p>
+          </div>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "amount",
+    header: "Amount",
+    cell: ({ getValue }) => {
+      const amount = getValue() as number;
+      return <span>£{amount.toFixed(2)}</span>;
+    },
+  },
+  {
+    accessorKey: "date",
+    header: "Date",
+    cell: ({ getValue }) => {
+      const dateStr = getValue() as string;
+      const date = new Date(dateStr);
+      return <div>{date.toLocaleDateString()}</div>;
+    },
+  },
+];
 
 const TransactionsGrid = () => {
   const { data: transactions, isLoading } = useTransactions();
   console.log(transactions, isLoading);
-  return <div>Transactions Grid</div>;
+
+  const table = useReactTable<Transaction>({
+    data: transactions,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+  });
+
+  return (
+    <div>
+      <Table className="mt-4 bg-card rounded-2xl">
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <TableHead key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext(),
+                      )}
+                </TableHead>
+              ))}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+      <div className="flex items-center justify-start space-x-2 py-4">
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.previousPage()}
+          disabled={!table.getCanPreviousPage()}
+        >
+          Previous
+        </Button>
+        <Button
+          variant="outline"
+          size="sm"
+          onClick={() => table.nextPage()}
+          disabled={!table.getCanNextPage()}
+        >
+          Next
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default TransactionsGrid;
