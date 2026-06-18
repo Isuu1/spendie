63cda0ec54300766a12a2cde90793d11768bde4d
Isuu1 <jakub.babelek@gmail.com>
2026-06-18 09:59:26 +0100

    Remove stylesheet from Footer

--- features/landing-page/components/Footer.tsx
+++ features/landing-page/components/Footer.tsx
@@ -1,27 +1,30 @@
-import React from "react";
-//Styles
-import styles from "./Footer.module.scss";
-
 const Footer = () => {
   return (
-    <div className={styles.footer}>
-      <div className={styles.footerTop}>
-        <div className={styles.footerContent}>
-          <h2>Spendie.</h2>
-          {/* <p>Your one-stop solution for managing your expenses.</p> */}
-        </div>
-        <ul className={styles.footerLinks}>
-          <li className={styles.footerLink}>Home</li>
-          <li className={styles.footerLink}>Features</li>
-          <li className={styles.footerLink}>About</li>
-          <li className={styles.footerLink}>Contact</li>
+    <div className="mt-16 mb-8">
+      <div className="flex justify-between items-center">
+        <h2>Spendie.</h2>
+        <ul className="list-none flex gap-4 flex-wrap text-base">
+          <li className="cursor-pointer hover:text-accent transition-colors">
+            Home
+          </li>
+          <li className="cursor-pointer hover:text-accent transition-colors">
+            Features
+          </li>
+          <li className="cursor-pointer hover:text-accent transition-colors">
+            About
+          </li>
+          <li className="cursor-pointer hover:text-accent transition-colors">
+            Contact
+          </li>
         </ul>
       </div>
 
-      <div className={styles.footerBottom}>
+      <div className="flex justify-between gap-2 mt-8">
         <p>© 2025 Spendie</p>
-        <p>Terms of Service</p>
-        <p>Privacy Policy</p>
+        <div className="flex gap-2">
+          <p>Terms of Service</p>
+          <p>Privacy Policy</p>
+        </div>
       </div>
     </div>
   );
