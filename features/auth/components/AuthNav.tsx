b199ca3bc56e7c414f38e513fe0ac491a77b6092
Isuu1 <jakub.babelek@gmail.com>
2026-06-19 07:04:07 +0100

    Style auth components

--- features/auth/components/AuthNav.tsx
+++ features/auth/components/AuthNav.tsx
@@ -8,9 +8,9 @@ const AuthNav = () => {
   const pathname = usePathname();
   const router = useRouter();
   return (
-    <nav className="relative flex gap-4 bg-bg-surface rounded-md list-none px-2 py-1">
+    <nav className="relative flex gap-4 bg-background rounded-md list-none px-2 py-1">
       <motion.span
-        className="z-1 absolute top-0 left-0 bottom-0 w-1/2 h-full bg-brand rounded-md"
+        className="z-1 absolute top-0 left-0 bottom-0 w-1/2 h-full bg-accent rounded-md"
         animate={{ x: pathname.includes("/login") ? "0%" : "100%" }}
         initial={false} // Prevent animation on initial render
         transition={{ type: "spring", stiffness: 700, damping: 30 }}
--- features/auth/layouts/AuthLayout.tsx
+++ features/auth/layouts/AuthLayout.tsx
@@ -9,10 +9,10 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
   return (
     <div className="w-full h-screen flex items-center justify-center bg-bg-primary">
-      <div className="flex flex-col items-center justify-center gap-6 max-w-175 bg-bg-secondary rounded-lg p-6">
-        <h1 className="absolute top-6 left-6 text-brand!">
+      <div className="flex flex-col items-center justify-center gap-6 max-w-175 bg-card rounded-2xl p-6">
+        <h2 className="absolute top-6 left-6 text-accent!">
           <Link href="/">Spendie.</Link>
-        </h1>
+        </h2>
         {pathname !== "/signup/success" &&
           pathname !== "/signup/account-confirmed" && <AuthNav />}
         {children}
