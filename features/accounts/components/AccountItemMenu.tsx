91de3c1883274c82e0931b0f42b58fc60cd75f68
Isuu1 <jakub.babelek@gmail.com>
2026-08-23 09:51:11 +0100

    Remove disconnect account functionality

--- features/accounts/components/AccountItemMenu.tsx
+++ features/accounts/components/AccountItemMenu.tsx
@@ -1,41 +1,28 @@
 "use client";
 
-import { useState } from "react";
 //Components
-import ConfirmAction from "@/shared/components/ConfirmAction";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-//Animations
-import { AnimatePresence } from "motion/react";
 //Icons
-import { EllipsisVertical, EyeOff, FilePenLine, Unplug } from "lucide-react";
+import { EllipsisVertical, EyeOff, FilePenLine } from "lucide-react";
 
 type AccountItemMenuProps = {
   onRename: () => void;
-  onDisconnect: () => void;
   onHide: () => void;
   isHidden: boolean;
-  isDisconnected: boolean;
 };
 
 const AccountItemMenu = ({
   onRename,
-  onDisconnect,
   onHide,
   isHidden,
-  isDisconnected,
 }: AccountItemMenuProps) => {
-  const [disconnectModalOpen, setDisconnectModalOpen] = useState(false);
-
   const handleRename = () => {
     onRename();
-  };
-  const handleDisconnect = () => {
-    setDisconnectModalOpen(true);
   };
 
   const handleHide = () => {
@@ -59,13 +46,6 @@ const AccountItemMenu = ({
             Rename
           </DropdownMenuItem>
           <DropdownMenuItem
-            onSelect={handleDisconnect}
-            className="cursor-pointer! hover:bg-bg-surface-dark-hover! text-white!"
-          >
-            <Unplug color="white" />
-            {isDisconnected ? "Reconnect" : "Disconnect"}
-          </DropdownMenuItem>
-          <DropdownMenuItem
             onSelect={handleHide}
             className="cursor-pointer! hover:bg-bg-surface-dark-hover! text-white!"
           >
@@ -74,28 +54,6 @@ const AccountItemMenu = ({
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
-
-      <AnimatePresence>
-        {disconnectModalOpen && (
-          <ConfirmAction
-            title={
-              !isDisconnected
-                ? "Disconnect this account?"
-                : "Reconnect this account?"
-            }
-            subtitle={
-              !isDisconnected
-                ? "You will no longer receive updates from this bank. Your existing data will remain."
-                : "This will attempt to reconnect the account and fetch the latest data."
-            }
-            onConfirm={() => {
-              onDisconnect();
-              setDisconnectModalOpen(false);
-            }}
-            onCancel={() => setDisconnectModalOpen(false)}
-          />
-        )}
-      </AnimatePresence>
     </>
   );
 };
