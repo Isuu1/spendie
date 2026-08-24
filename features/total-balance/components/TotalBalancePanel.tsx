508cc0c380ef7d8c6485361498d30474ce95eaad
Isuu1 <jakub.babelek@gmail.com>
2026-08-24 14:07:47 +0100

    Remove is_disconnected from total balance calculation + add utility to calculate balances

--- features/total-balance/components/TotalBalancePanel.tsx
+++ features/total-balance/components/TotalBalancePanel.tsx
@@ -15,6 +15,7 @@ import { getTransactionsInPeriod } from "@/features/transactions/lib/utils/getTr
 import { FutureBalanceProvider } from "@/features/future-balance/context/FutureBalanceContext";
 import { ChevronDown, ChevronUp } from "lucide-react";
 import { Amount } from "@/shared/components/Amount";
+import { calculateBalances } from "../lib/calculateBalances";
 
 const TotalBalancePanel = () => {
   const { data = [], isLoading } = useAccounts();
@@ -25,13 +26,7 @@ const TotalBalancePanel = () => {
     return <DashboardPanelLoader height={218} />;
   }
 
-  const totalBalance = data?.reduce((sum: number, currentAccount: Account) => {
-    if (currentAccount.is_disconnected) {
-      return sum;
-    }
-    const currentBalance = currentAccount.current_balance ?? 0;
-    return sum + currentBalance;
-  }, 0);
+  const { active, hidden } = calculateBalances(data as Account[]);
 
   const startOfMonth = dayjs().startOf("month").toDate();
   const endOfMonth = dayjs().endOf("month").toDate();
@@ -51,24 +46,29 @@ const TotalBalancePanel = () => {
       <div className="flex flex-row justify-between items-center gap-5">
         <h4 className="text-secondary">Total Balance</h4>
       </div>
-
-      <Amount amount={totalBalance ?? 0} className="text-2xl font-semibold" />
+      <div className="flex gap-2 justify-between items-center">
+        <Amount amount={active ?? 0} className="text-2xl font-semibold" />
+        <div className="flex gap-2">
+          <p className="text-sm text-secondary">Hidden accounts:</p>
+          <Amount amount={hidden ?? 0} className="text-sm text-secondary" />
+        </div>
+      </div>
 
       <div className="flex flex-row gap-6 justify-between items-center">
         <p className="text-secondary">This month</p>
         <div className="flex flex-row gap-3">
-          <p className="flex items-center gap-1 text-green-600">
+          <p className="flex items-center gap-1 text-green-600 font-semibold">
             <ChevronUp size={17} />
             <Amount amount={income} />
           </p>
-          <div className="flex items-center gap-1 text-red-600">
+          <div className="flex items-center gap-1 text-red-600 font-semibold">
             <ChevronDown size={17} />
             <Amount amount={expenses} />
           </div>
         </div>
       </div>
 
-      <FutureBalanceProvider totalBalance={totalBalance ?? 0}>
+      <FutureBalanceProvider totalBalance={active ?? 0}>
         <FutureBalance />
       </FutureBalanceProvider>
     </div>
